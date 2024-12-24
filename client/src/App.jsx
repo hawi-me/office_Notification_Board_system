0b697de12d644bb430ef30be08d72b463da696b6
hawi-me <hawiabdi41@gmail.com>
2024-12-24 23:54:10 +0400

    working enviroment

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='tex-3xl text-red-500'>this is the office notice board</h1>
+    </div>
+  )
+}
