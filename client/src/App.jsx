0ac83c119899a0448f84061688bb3b5f1e72bba7
hawi-me <hawiabdi41@gmail.com>
2024-12-25 01:29:44 +0400

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePage from './pages/publicpage/homepage'
+import LoginPage from './pages/publicpage/LoginPage'
+import SignupPage from './pages/publicpage/SignupPage'
+import PostDetails from './pages/publicpage/PostDetails'
+import AdminDashboard from './pages/Adminpages/Admin'
+import PostManagementPage from './pages/Adminpages/postMangemnetPage'
+import UserManagementPage from './pages/Adminpages/UserMangemnetPage'
+import SystemSettingsPage from './pages/Adminpages/Systemsetting'
+import EditorDashboard from './pages/Editors/Editordashboard'
+import CreatePostPage from './pages/Editors/CreatePost'
+import SchedulePostPage from './pages/Editors/Schedule'
+import CommentsPage from './pages/Authenticateduser/CommentPage'
+import UserProfilePage from './pages/Authenticateduser/UserProfile'
 
 export default function App() {
   return (
-    <div>
-      <h1 className='tex-3xl text-red-500'>this is the office notice board</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/posts/:postId" element={<PostDetails />} />
+  
+      {/* Admin Routes */}
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/admin/users" element={<UserManagementPage />} />
+      <Route path="/admin/posts" element={<PostManagementPage />} />
+      <Route path="/admin/settings" element={<SystemSettingsPage />} />
+  
+      {/* Editor Routes */}
+      <Route path="/editor" element={<EditorDashboard />} />
+      <Route path="/editor/create" element={<CreatePostPage />} />
+      <Route path="/editor/schedule" element={<SchedulePostPage />} />
+  
+      {/* Authenticated User Routes */}
+      <Route path="/comments" element={<CommentsPage />} />
+      <Route path="/profile" element={<UserProfilePage />} />
+  
+      {/* Utility Routes */}
+      {/* <Route path="*" element={<NotFoundPage />} /> */}
+    </Routes>
+  </BrowserRouter>
+  
   )
 }
---
+++ client/src/pages/publicpage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+        <h1>this home page</h1>
+      
+    </div>
+  )
+}
